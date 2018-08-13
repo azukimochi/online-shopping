64c0eae4dfd1c091b18a4f28d7f3364fd51f9b8b
Durian <karen.kua7@gmail.com>
2018-08-12 23:29:02 -0400

    Updated js file with more functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,10 +25,56 @@ function readProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            console.log(`ID: ${res[i].item_id}|| Product: ${res[i].product_name}|| Department: ${res[i].department_name}|| Price: $${res[i].price}|| Stock: ${res[i].stock_quantity}\n`);
+            console.log(`ID: ${res[i].item_id}|| Product: ${res[i].product_name}|| Department: ${res[i].department_name}|| Price: $${res[i].price}\n`);
         }
+        selectID()
     });
 }
 
+function selectID() {
+    inquirer
+        .prompt({
+            name: "id",
+            type: "input",
+            message: "Type in the ID of the product you would like to purchase:"
+        }).then(function(answer) {
+            var itemID = answer.id;
+            var query = "SELECT * FROM products WHERE item_id=?";
+            connection.query(query, [itemID], function(err, res, fields) {
+                for (var i = 0; i < res.length; i++) {
+                    console.log(`You selected ID ${itemID}. The item's information is below: \n`);
+                    console.log(`Product: ${res[i].product_name}|| Department: ${res[i].department_name}|| Price: $${res[i].price}\n`);
+                    var availableStock = res[i].stock_quantity;
+                } 
+                requestPurchase(itemID, availableStock);
+            });
+        });
+}
+
+function requestPurchase(itemID, availableStock) {
+    inquirer
+    .prompt({
+        name: "units",
+        type: "input",
+        message: "How many units of this product would you like to purchase?"
+    }).then(function(answer) {
+        var requestedUnits = answer.units;
+            console.log(`Number of units you requested: ${requestedUnits}`);
+            if (requestedUnits > availableStock) {
+                console.log(`Insufficient quantities!  Try again with another number.  There are only ${availableStock} in stock.`)
+            } else {
+                processPurchase()
+            }
+        });
+}
+
+function processPurchase() {
+
+}
+
+
+
+
 
 // connection.end();
+
